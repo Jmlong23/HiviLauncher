29b2aab213a97215ea5bb0b11d4b8ae83c411500
LIeHiuTunG <1154201280@qq.com>
2026-07-15 16:51:13 +0800

    修改主界面的绘制

--- app/src/main/java/com/hivi/launcher/main/ui/MainActivity.java
+++ app/src/main/java/com/hivi/launcher/main/ui/MainActivity.java
@@ -8,24 +8,16 @@ import android.content.IntentFilter;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 
 import com.hivi.launcher.R;
 import com.hivi.launcher.account.ui.AuthorizationDialog;
 import com.hivi.launcher.base.BaseActivity;
-import com.hivi.launcher.customview.AppIconDrawable;
-import com.hivi.launcher.customview.CircleDrawable;
-import com.hivi.launcher.customview.PlayDrawable;
-import com.hivi.launcher.customview.RoundRectDrawable;
-import com.hivi.launcher.customview.StatusIconDrawable;
 import com.hivi.launcher.databinding.ActivityMainBinding;
 import com.hivi.launcher.main.presenter.MainPresenter;
 import com.hivi.launcher.music.ui.MusicActivity;
 import com.hivi.launcher.systemapps.ui.SystemAppsActivity;
-import com.hivi.launcher.utils.UiUtils;
 
 public class MainActivity extends BaseActivity<ActivityMainBinding, MainPresenter>
         implements MainView {
@@ -52,8 +44,6 @@ public class MainActivity extends BaseActivity<ActivityMainBinding, MainPresente
 
     @Override
     protected void initView(@Nullable Bundle savedInstanceState) {
-        applyScaledLayout();
-        applyMainBackgrounds();
         bindMainClickListeners();
     }
 
@@ -163,100 +153,6 @@ public class MainActivity extends BaseActivity<ActivityMainBinding, MainPresente
         binding.switchText.setText(R.string.main_switch);
     }
 
-    private void applyScaledLayout() {
-        binding.launcherRoot.setPadding(ui(50), ui(34), ui(50), ui(26));
-
-        setLinearLayoutSize(binding.topBar, ViewGroup.LayoutParams.MATCH_PARENT, ui(54));
-        setLinearLayoutSize(binding.topToCardsGap, ViewGroup.LayoutParams.MATCH_PARENT, ui(104));
-        setLinearLayoutSize(binding.cardsRow, ViewGroup.LayoutParams.MATCH_PARENT, ui(325));
-        setLinearLayoutSize(binding.cardsBottomGap, ViewGroup.LayoutParams.MATCH_PARENT, ui(38));
-        setLinearLayoutSize(binding.bottomBar, ViewGroup.LayoutParams.MATCH_PARENT, ui(190));
-
-        setLinearLayoutSize(binding.wifiIcon, ui(44), ui(44));
-        setLinearLayoutSize(binding.wifiIconTextGap, ui(8), ui(1));
-        setLinearLayoutSize(binding.wifiBtGap, ui(24), ui(1));
-        setLinearLayoutSize(binding.bluetoothIcon, ui(44), ui(44));
-        setLinearLayoutSize(binding.bluetoothIconTextGap, ui(8), ui(1));
-        setWeightedLinearLayoutSize(binding.topBarFiller, 0, ui(1), 1f, 0, 0, 0, 0);
-        setLinearLayoutSize(binding.modeText, ui(216), ui(54));
-        setLinearLayoutSize(binding.topActionGap, ui(26), ui(1));
-        setLinearLayoutSize(binding.accountText, ui(147), ui(54));
-
-        setWeightedLinearLayoutSize(binding.clockCard, 0, ViewGroup.LayoutParams.MATCH_PARENT,
-                1f, 0, 0, 0, 0);
-        setWeightedLinearLayoutSize(binding.weatherCard, 0, ViewGroup.LayoutParams.MATCH_PARENT,
-                1f, ui(40), 0, ui(40), 0);
-        setWeightedLinearLayoutSize(binding.volumeCard, 0, ViewGroup.LayoutParams.MATCH_PARENT,
-                1f, 0, 0, 0, 0);
-
-        applyCardStyle(binding.clockCard);
-        applyCardStyle(binding.weatherCard);
-        applyCardStyle(binding.volumeCard);
-
-        setLinearLayoutSize(binding.clockFace, ui(170), ui(170));
-        setLinearLayoutSize(binding.timeGap, ui(16), ui(1));
-        setLinearLayoutSize(binding.dateText, ViewGroup.LayoutParams.MATCH_PARENT, ui(46));
-        setLinearLayoutMargins(binding.dateText, ui(26), ui(12), ui(26), 0);
-
-        setLinearLayoutSize(binding.weatherIcon, ui(170), ui(122));
-        setLinearLayoutSize(binding.weatherGap, ui(24), ui(1));
-        setLinearLayoutSize(binding.cityText, ViewGroup.LayoutParams.MATCH_PARENT, ui(46));
-        setLinearLayoutMargins(binding.cityText, ui(26), ui(12), ui(26), 0);
-
-        setLinearLayoutSize(binding.volumeDialView, ui(190), ui(190));
-        setLinearLayoutSize(binding.volumeGap, ui(28), ui(1));
-        setLinearLayoutSize(binding.volumeControls, ViewGroup.LayoutParams.WRAP_CONTENT, ui(42));
-        setLinearLayoutMargins(binding.volumeControls, 0, ui(12), 0, 0);
-        setLinearLayoutSize(binding.volumeUpButton, ui(42), ui(42));
-        setLinearLayoutSize(binding.volumeAdjustLabel, ui(92), ui(42));
-        setLinearLayoutSize(binding.volumeDownButton, ui(42), ui(42));
-
-        setWeightedLinearLayoutSize(binding.playerContainer, 0, ViewGroup.LayoutParams.MATCH_PARENT,
-                1f, 0, 0, ui(99), 0);
-        binding.playerContainer.setPadding(ui(28), 0, ui(28), 0);
-        setLinearLayoutSize(binding.recordView, ui(154), ui(154));
-        setWeightedLinearLayoutSize(binding.musicTextBox, 0, ViewGroup.LayoutParams.MATCH_PARENT,
-                1f, ui(26), 0, ui(18), 0);
-        setLinearLayoutSize(binding.playIcon, ui(54), ui(54));
-
-        setLinearLayoutSize(binding.settingsButton, ui(112), ui(176));
-        setLinearLayoutSize(binding.settingsIcon, ui(112), ui(112));
-        setLinearLayoutSize(binding.settingsText, ViewGroup.LayoutParams.MATCH_PARENT, ui(46));
-        setLinearLayoutSize(binding.settingsScreensaverGap, ui(92), ui(1));
-        setLinearLayoutSize(binding.screensaverButton, ui(112), ui(176));
-        setLinearLayoutSize(binding.screensaverIcon, ui(112), ui(112));
-        setLinearLayoutSize(binding.screensaverText, ViewGroup.LayoutParams.MATCH_PARENT, ui(46));
-        setLinearLayoutSize(binding.screensaverSwitchGap, ui(92), ui(1));
-        setLinearLayoutSize(binding.switchButton, ui(112), ui(176));
-        setLinearLayoutSize(binding.switchIcon, ui(112), ui(112));
-        setLinearLayoutSize(binding.switchText, ViewGroup.LayoutParams.MATCH_PARENT, ui(46));
-    }
-
-    private void applyMainBackgrounds() {
-        binding.wifiIcon.setImageDrawable(new StatusIconDrawable("wifi"));
-        binding.bluetoothIcon.setImageDrawable(new StatusIconDrawable("bt"));
-        binding.modeText.setBackground(new RoundRectDrawable(0x66454545, ui(24)));
-        binding.accountText.setBackground(new RoundRectDrawable(0x66454545, ui(24)));
-        binding.accountText.setCompoundDrawablesWithIntrinsicBounds(
-                new CircleDrawable(0xffd9a35c), null, null, null);
-        binding.accountText.setCompoundDrawablePadding(dp(8));
-
-        binding.dateText.setBackground(new RoundRectDrawable(0x66454545, ui(24)));
-        binding.cityText.setBackground(new RoundRectDrawable(0x66454545, ui(24)));
-        binding.volumeAdjustLabel.setBackground(new RoundRectDrawable(0x66454545, ui(24)));
-        binding.volumeUpButton.setBackground(new RoundRectDrawable(0xff454545, ui(14)));
-        binding.volumeDownButton.setBackground(new RoundRectDrawable(0xff454545, ui(14)));
-
-        binding.playerContainer.setBackground(new RoundRectDrawable(0xff45474a, ui(20)));
-        binding.playIcon.setImageDrawable(new PlayDrawable());
-        binding.settingsIcon.setImageDrawable(new AppIconDrawable("gear"));
-        binding.screensaverIcon.setImageDrawable(new AppIconDrawable("image"));
-        binding.switchIcon.setImageDrawable(new AppIconDrawable("switch"));
-        binding.settingsButton.setBackground(new RoundRectDrawable(0x00303030, ui(16)));
-        binding.screensaverButton.setBackground(new RoundRectDrawable(0x00303030, ui(16)));
-        binding.switchButton.setBackground(new RoundRectDrawable(0x00303030, ui(16)));
-    }
-
     private void bindMainClickListeners() {
         binding.accountText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -302,11 +198,6 @@ public class MainActivity extends BaseActivity<ActivityMainBinding, MainPresente
         });
     }
 
-    private void applyCardStyle(LinearLayout card) {
-        card.setPadding(ui(26), ui(14), ui(26), ui(18));
-        card.setBackground(new RoundRectDrawable(0xff3f3f3f, ui(18)));
-    }
-
     private void registerSystemReceiver() {
         IntentFilter filter = new IntentFilter();
         filter.addAction(android.net.wifi.WifiManager.NETWORK_STATE_CHANGED_ACTION);
@@ -323,24 +214,4 @@ public class MainActivity extends BaseActivity<ActivityMainBinding, MainPresente
         }
     }
 
-    private int ui(int value) {
-        return UiUtils.ui(this, value);
-    }
-
-    private int dp(int value) {
-        return UiUtils.dp(this, value);
-    }
-
-    private void setLinearLayoutSize(View view, int width, int height) {
-        UiUtils.setLinearLayoutSize(view, width, height);
-    }
-
-    private void setWeightedLinearLayoutSize(View view, int width, int height, float weight,
-            int left, int top, int right, int bottom) {
-        UiUtils.setWeightedLinearLayoutSize(view, width, height, weight, left, top, right, bottom);
-    }
-
-    private void setLinearLayoutMargins(View view, int left, int top, int right, int bottom) {
-        UiUtils.setLinearLayoutMargins(view, left, top, right, bottom);
-    }
 }
