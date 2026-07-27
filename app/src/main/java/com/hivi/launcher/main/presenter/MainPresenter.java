d0993b1fefe2a1a917e05a87ed3455b648ea4c06
Mrlong <543196687@qq.com>
2026-07-27 01:44:07 -0700

    Fix bottom navigation recents action

--- app/src/main/java/com/hivi/launcher/main/presenter/MainPresenter.java
+++ app/src/main/java/com/hivi/launcher/main/presenter/MainPresenter.java
@@ -1,11 +1,13 @@
 package com.hivi.launcher.main.presenter;
 
-import android.content.ActivityNotFoundException;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.bluetooth.BluetoothDevice;
+import android.os.SystemClock;
 import android.util.Log;
+import android.view.InputDevice;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
 
 import com.hivi.launcher.base.BasePresenter;
 import com.hivi.launcher.main.model.MainStatusRepository;
@@ -15,6 +17,7 @@ import com.hivi.launcher.music.model.BluetoothMediaController;
 import com.hivi.launcher.systemapps.ui.SystemAppsActivity;
 
 import java.text.SimpleDateFormat;
+import java.lang.reflect.Method;
 import java.util.ArrayDeque;
 import java.util.Date;
 import java.util.Deque;
@@ -22,8 +25,7 @@ import java.util.Locale;
 
 public class MainPresenter extends BasePresenter<MainView> {
     public static final String ACTION_VOLUME_CHANGED = "android.media.VOLUME_CHANGED_ACTION";
-    private static final ComponentName RECENTS_ACTIVITY = new ComponentName(
-            "com.android.launcher3", "com.android.quickstep.RecentsActivity");
+    private static final int INJECT_INPUT_EVENT_MODE_ASYNC = 0;
 
     private final Context mContext;
     private final MainStatusRepository mStatusRepository;
@@ -140,14 +142,30 @@ public class MainPresenter extends BasePresenter<MainView> {
 
     public void onBottomNavigationRecentsClicked() {
         Log.d(TAG, "Bottom navigation recents clicked");
-        Intent intent = new Intent(Intent.ACTION_MAIN)
-                .addCategory(Intent.CATEGORY_DEFAULT)
-                .setComponent(RECENTS_ACTIVITY)
-                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        long downTime = SystemClock.uptimeMillis();
+        boolean downInjected = injectSystemKeyEvent(KeyEvent.ACTION_DOWN, downTime, downTime);
+        boolean upInjected = injectSystemKeyEvent(KeyEvent.ACTION_UP, downTime,
+                SystemClock.uptimeMillis());
+        if (!downInjected || !upInjected) {
+            Log.e(TAG, "Unable to inject recent apps key event");
+        }
+    }
+
+    private boolean injectSystemKeyEvent(int action, long downTime, long eventTime) {
+        KeyEvent event = new KeyEvent(downTime, eventTime, action, KeyEvent.KEYCODE_APP_SWITCH,
+                0, 0, KeyCharacterMap.VIRTUAL_KEYBOARD, 0, KeyEvent.FLAG_FROM_SYSTEM,
+                InputDevice.SOURCE_KEYBOARD);
         try {
-            mContext.startActivity(intent);
-        } catch (ActivityNotFoundException | SecurityException e) {
-            Log.e(TAG, "Unable to open recent apps", e);
+            Class<?> inputManagerClass = Class.forName("android.hardware.input.InputManager");
+            Object inputManager = inputManagerClass.getMethod("getInstance").invoke(null);
+            Method injectInputEvent = inputManagerClass.getMethod("injectInputEvent",
+                    Class.forName("android.view.InputEvent"), int.class);
+            Object injected = injectInputEvent.invoke(inputManager, event,
+                    INJECT_INPUT_EVENT_MODE_ASYNC);
+            return injected instanceof Boolean && (Boolean) injected;
+        } catch (ReflectiveOperationException | SecurityException e) {
+            Log.e(TAG, "Unable to inject system key event", e);
+            return false;
         }
     }
 
